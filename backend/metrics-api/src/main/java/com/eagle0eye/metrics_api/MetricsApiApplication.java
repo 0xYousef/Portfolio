41590f30f879723ef5a4bdf195f8d87bcfe9cc18
Yousef Mohaned <youssief192678@fci.bu.edu.eg>
2025-10-29 17:10:47 +0300

    build Architecture #1

---
+++ backend/metrics-api/src/main/java/com/eagle0eye/metrics_api/MetricsApiApplication.java
@@ -0,0 +1,13 @@
+package com.eagle0eye.metrics_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetricsApiApplication.class, args);
+	}
+
+}
